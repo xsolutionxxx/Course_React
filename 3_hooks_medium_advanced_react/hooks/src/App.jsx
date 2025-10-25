681337b2c44841ea80dab87bb6e8e19256af7532
Nazar <nazartalaievych@gmail.com>
2025-10-25 21:23:59 +0300

    useState

---
+++ 3_hooks_medium_advanced_react/hooks/src/App.jsx
@@ -0,0 +1,135 @@
+import { Component, useState } from "react";
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+/* class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      autoplay: false,
+      slide: 0,
+    };
+  }
+
+  changeSlide = (i) => {
+    this.setState(({ slide }) => ({
+      slide: slide + i,
+    }));
+  };
+
+  toggleAutoplay = () => {
+    this.setState(({ autoplay }) => ({
+      autoplay: !autoplay,
+    }));
+  };
+
+  render() {
+    return (
+      <Container>
+        <div className="slider w-50 m-auto">
+          <img
+            className="d-block w-100"
+            src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+            alt="slide"
+          />
+          <div className="text-center mt-5">
+            Active slide {this.state.slide} <br />{" "}
+            {this.state.autoplay ? "auto" : null}
+          </div>
+          <div className="buttons mt-3">
+            <button
+              className="btn btn-primary me-2"
+              onClick={() => this.changeSlide(-1)}
+            >
+              -1
+            </button>
+            <button
+              className="btn btn-primary me-2"
+              onClick={() => this.changeSlide(1)}
+            >
+              +1
+            </button>
+            <button
+              className="btn btn-primary me-2"
+              onClick={this.toggleAutoplay}
+            >
+              toggle autoplay
+            </button>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+} */
+
+const clacValue = () => {
+  console.log("random");
+
+  return Math.floor(Math.random() * (50 - 1) + 1);
+};
+
+const Slider = () => {
+  /* const slideStateArray = useState();
+  console.log(slideStateArray); */
+
+  const [slide, setSlide] = useState(clacValue());
+  const [autoplay, setAutoplay] = useState(false);
+  /* const [state, setState] = useState({ slide: 0, autoplay: false }); */
+
+  function changeSlide(i) {
+    setSlide((slide) => slide + i);
+    /* setSlide((slide) => slide + i);
+    setSlide(slide + i);
+    setSlide(slide + i); */
+  }
+
+  function toggleAutoplay() {
+    setAutoplay((autoplay) => !autoplay);
+  }
+
+  /* function changeSlide(i) {
+    setState((state) => ({ ...state, slide: state.slide + i }));
+  }
+
+  function toggleAutoplay() {
+    setState((state) => ({ ...state, autoplay: !state.autoplay }));
+  } */
+
+  return (
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img
+          className="d-block w-100"
+          src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+          alt="slide"
+        />
+        <div className="text-center mt-5">
+          Active slide {slide} <br /> {autoplay ? "auto" : null}
+        </div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(-1)}
+          >
+            -1
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(1)}
+          >
+            +1
+          </button>
+          <button className="btn btn-primary me-2" onClick={toggleAutoplay}>
+            toggle autoplay
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+function App() {
+  return <Slider />;
+}
+
+export default App;
